b80e200b015a954f8aeede5baebff15b9519b126
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2025-01-23 21:54:06 +0500

    msg

---
+++ components/add.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const addScheme = new mongoose.Schema({
+  image: String,
+  heading: String,
+  detail: String,
+  price: String,
+  category: String,
+  subCategory: String,
+});
+
+const Add = mongoose.model('Add', addScheme);
+
+module.exports = Add;
